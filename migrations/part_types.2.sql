90051d234a2d0d023bc407f92c60f2bac50d4440
mbs9 <66876095+MBS9@users.noreply.github.com>
2026-08-23 16:49:40 +0200

    feat: add URL parsing

--- migrations/part_types.2.sql
+++ migrations/part_types.2.sql
@@ -2,6 +2,6 @@ INSERT INTO part_types (name, description) VALUES
 ('method', 'HTTP Method (GET, POST, etc.)'),
 ('host', 'The host of the request'),
 ('query_part', 'Part of the query part of the URL'),
-('endpoint_part', 'Part of the endpoint path of the URL'),
+('path_part', 'Part of the endpoint path of the URL'),
 ('body_part', 'Part of the body of the request'),
 ('header', 'HTTP Header (User-Agent, Content-Type, etc.)');
--- upload/json.c
+++ upload/json.c
@@ -1,4 +1,10 @@
 #include <json-c/json.h>
+#define _DEFAULT_SOURCE
+#include <string.h>
+
+char *SPECIAL_URL_KEY = "path";
+char *SPECIAL_OUTPUT_URL_KEY = "path_part";
+char *SPECIAL_SEPARATOR = "/";
 
 int process_json(const char *json_str, char **output_str)
 {
@@ -13,11 +19,30 @@ int process_json(const char *json_str, char **output_str)
 
     json_object_object_foreach(json_obj, key, val)
     {
-        json_object_get(val);
-        json_object *item = json_object_new_object();
-        json_object_object_add(item, "part_type", json_object_new_string(key));
-        json_object_object_add(item, "data", val);
-        json_object_array_add(array, item);
+        if (strcmp(key, SPECIAL_URL_KEY) == 0)
+        {
+            // Split up the path into parts and add them to the output JSON
+            const char *path = json_object_get_string(val);
+            char *path_copy = strdup(path);
+            char *next_part = strtok(path_copy, SPECIAL_SEPARATOR);
+            while (next_part != NULL)
+            {
+                json_object *item = json_object_new_object();
+                json_object_object_add(item, "part_type", json_object_new_string(SPECIAL_OUTPUT_URL_KEY));
+                json_object_object_add(item, "data", json_object_new_string(next_part));
+                json_object_array_add(array, item);
+                next_part = strtok(NULL, SPECIAL_SEPARATOR);
+            }
+            free(path_copy);
+        }
+        else
+        {
+            json_object_get(val);
+            json_object *item = json_object_new_object();
+            json_object_object_add(item, "part_type", json_object_new_string(key));
+            json_object_object_add(item, "data", val);
+            json_object_array_add(array, item);
+        }
     }
 
     json_object_object_add(output, "parts", array);
